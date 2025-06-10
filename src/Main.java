8d478d3d2005faf6f730ff51c26fcfc08a66f286
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-06-10 23:48:33 +0700

    oop with constructor

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle();
+        System.out.println("chieu dai : "+ rec1.getLength());
+        System.out.println("chieu rong: "+ rec1.getWidth());
+        System.out.println("chu vi is: "+ rec1.getChuVi());
+        System.out.println("dien tich is: "+ rec1.getDienTich());
+
+        System.out.println();
+
+        Rectangle rec2 = new Rectangle(4,3);
+        System.out.println("chieu dai : "+ rec2.getLength());
+        System.out.println("chieu rong: "+ rec2.getWidth());
+        System.out.println("chu vi is: "+ rec2.getChuVi());
+        System.out.println("dien tich is: "+ rec2.getDienTich());
+    }
+}
+
+class Rectangle{
+    private int length;
+    private int width;
+
+    public Rectangle(){
+        length = 1;
+        width = 1;
+    }
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    int getChuVi(){
+        return (width + length) * 2;
+    }
+
+    int getDienTich(){
+        return width * length;
+    }
+
+    // (Nếu cần) Getter, to get in the private
+    public int getLength() {
+        return length;
+    }
+    public int getWidth() {
+        return width;
+    }
+
+}
